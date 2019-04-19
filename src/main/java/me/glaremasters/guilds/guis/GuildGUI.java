567cb3ec52d2d44cc5963bdeda88e3b05eb98344
darbyjack <admin@glaremasters.me>
2019-04-19 10:20:18 -0500

    Created the outline panes for the management GUI

--- src/main/java/me/glaremasters/guilds/guis/GuildGUI.java
+++ src/main/java/me/glaremasters/guilds/guis/GuildGUI.java
@@ -4,11 +4,16 @@ import ch.jalu.configme.SettingsManager;
 import co.aikar.commands.ACFBukkitUtil;
 import co.aikar.commands.CommandManager;
 import com.github.stefvanschie.inventoryframework.Gui;
-import com.github.stefvanschie.inventoryframework.pane.StaticPane;
+import com.github.stefvanschie.inventoryframework.GuiItem;
+import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
+import com.github.stefvanschie.inventoryframework.pane.Pane;
 import lombok.AllArgsConstructor;
 import me.glaremasters.guilds.Guilds;
 import me.glaremasters.guilds.configuration.sections.GuildManageSettings;
 import me.glaremasters.guilds.guild.GuildHandler;
+import me.glaremasters.guilds.utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Glare
@@ -25,11 +30,23 @@ public class GuildGUI {
 
     public Gui getGuildGUI() {
 
-        Gui gui = new Gui(guilds, 1, ACFBukkitUtil.color(settingsManager.getProperty(GuildManageSettings.MANAGEMENT_NAME)));
+        Gui gui = new Gui(guilds, 3, ACFBukkitUtil.color(settingsManager.getProperty(GuildManageSettings.MANAGEMENT_NAME)));
 
-        StaticPane pane = new StaticPane(0, 0, 9 ,1);
+        OutlinePane pane = new OutlinePane(0, 0, 9, 3, Pane.Priority.LOW);
+
+        createPanes(pane);
+
+        gui.addPane(pane);
+
 
         return gui;
+    }
+
+    public void createPanes(OutlinePane pane) {
+        ItemBuilder builder = new ItemBuilder(new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) 7));
+        for (int i = 0; i < 27; i++) {
+            pane.addItem(new GuiItem(builder.build(), event -> event.setCancelled(true)));
+        }
     }
 
 }
