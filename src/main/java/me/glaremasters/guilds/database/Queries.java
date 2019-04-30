dd66a98cad4bb7893d79096e29f910d3b8b04754
darbyjack <admin@glaremasters.me>
2019-04-30 10:36:22 -0500

    Updated the SQL queries

--- src/main/java/me/glaremasters/guilds/database/Queries.java
+++ src/main/java/me/glaremasters/guilds/database/Queries.java
@@ -10,64 +10,112 @@ import org.intellij.lang.annotations.Language;
 public class Queries {
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_GUILDS = "CREATE TABLE IF NOT EXISTS `guilds`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `name` varchar(60) UNIQUE NOT NULL,\n" +
-            "  `prefix` varchar(60) NOT NULL,\n" +
-            "  `master` varchar(36) NOT NULL,\n" +
-            "  `home` varchar(1000) NULL,\n" +
-            "  `skull` varchar(60) NOT NULL,\n" +
-            "  `status` varchar(10) NOT NULL,\n" +
-            "  `tier` integer(5) NOT NULL,\n" +
-            "  `balance` double NOT NULL\n" +
-            ");";
+    public static final String CREATE_TABLE_GUILDS = "CREATE TABLE IF NOT EXISTS `guilds` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `name` VARCHAR(60) NOT NULL,\n" +
+            "  `prefix` VARCHAR(60) NOT NULL,\n" +
+            "  `master` VARCHAR(36) NOT NULL,\n" +
+            "  `home` VARCHAR(100) NOT NULL,\n" +
+            "  `skull` VARCHAR(100) NOT NULL,\n" +
+            "  `status` VARCHAR(15) NOT NULL,\n" +
+            "  `tier` INT(5) NOT NULL,\n" +
+            "  `balance` DOUBLE NOT NULL,\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,\n" +
+            "  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)\n" +
+            "ENGINE = InnoDB;\n";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_MEMBERS = "CREATE TABLE IF NOT EXISTS `guild_members`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `members` TEXT NOT NULL,\n" +
-            "  `role` integer(5) NOT NULL\n" +
-            ");";
+    public static final String CREATE_TABLE_MEMBERS = "CREATE TABLE IF NOT EXISTS `guild_members` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `uuid` VARCHAR(36) NOT NULL,\n" +
+            "  `role` INT(5) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `id`\n" +
+            "    FOREIGN KEY (`id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;\n";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_PENDING_MEMBERS = "CREATE TABLE IF NOT EXISTS `pending_members`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `invites` TEXT\n" +
-            ");";
+    public static final String CREATE_TABLE_PENDING_MEMBERS = "CREATE TABLE IF NOT EXISTS `pending_members` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `invites` TEXT NOT NULL,\n" +
+            "  `guilds_id` VARCHAR(36) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`, `guilds_id`),\n" +
+            "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,\n" +
+            "  INDEX `fk_pending_members_guilds1_idx` (`guilds_id` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `fk_pending_members_guilds1`\n" +
+            "    FOREIGN KEY (`guilds_id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;\n";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_ALLIES = "CREATE TABLE IF NOT EXISTS `guild_allies`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `invites` TEXT\n" +
-            ");";
+    public static final String CREATE_TABLE_ALLIES = "CREATE TABLE IF NOT EXISTS `guild_allies` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `allies` TEXT NOT NULL,\n" +
+            "  `guilds_id` VARCHAR(36) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`, `guilds_id`),\n" +
+            "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,\n" +
+            "  INDEX `fk_guild_allies_guilds1_idx` (`guilds_id` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `fk_guild_allies_guilds1`\n" +
+            "    FOREIGN KEY (`guilds_id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;\n";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_PENDING_ALLIES = "CREATE TABLE IF NOT EXISTS `pending_allies`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `invites` TEXT\n" +
-            ");";
+    public static final String CREATE_TABLE_PENDING_ALLIES = "CREATE TABLE IF NOT EXISTS `pending_allies` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `invites` TEXT NOT NULL,\n" +
+            "  `guilds_id` VARCHAR(36) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`, `guilds_id`),\n" +
+            "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,\n" +
+            "  INDEX `fk_pending_allies_guilds1_idx` (`guilds_id` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `fk_pending_allies_guilds1`\n" +
+            "    FOREIGN KEY (`guilds_id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_CODES = "CREATE TABLE IF NOT EXISTS `guild_codes`\n" +
-            "(\n" +
-            "  `code` varchar(100) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `uses` integer(10) NOT NULL,\n" +
-            "  `creator` varchar(36) NOT NULL,\n" +
-            "  `redeemers` TEXT\n" +
-            ");";
+    public static final String CREATE_TABLE_CODES = "CREATE TABLE IF NOT EXISTS `guild_codes` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `code` VARCHAR(100) NOT NULL,\n" +
+            "  `uses` INT(10) NOT NULL,\n" +
+            "  `creator` VARCHAR(36) NOT NULL,\n" +
+            "  `redeemers` TEXT NULL,\n" +
+            "  `guilds_id` VARCHAR(36) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`, `guilds_id`),\n" +
+            "  UNIQUE INDEX `code_UNIQUE` (`code` ASC) VISIBLE,\n" +
+            "  INDEX `fk_guild_codes_guilds1_idx` (`guilds_id` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `fk_guild_codes_guilds1`\n" +
+            "    FOREIGN KEY (`guilds_id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;\n";
 
     @Language("MySQL")
-    public static final String CREATE_TABLE_VAULTS = "CREATE TABLE IF NOT EXISTS `guild_vaults`\n" +
-            "(\n" +
-            "  `id` varchar(36) UNIQUE PRIMARY KEY NOT NULL,\n" +
-            "  `vaults` TEXT NOT NULL\n" +
-            ");";
-
+    public static final String CREATE_TABLE_VAULTS = "CREATE TABLE IF NOT EXISTS `guild_vaults` (\n" +
+            "  `id` VARCHAR(36) NOT NULL,\n" +
+            "  `vaults` TEXT NOT NULL,\n" +
+            "  `guilds_id` VARCHAR(36) NOT NULL,\n" +
+            "  PRIMARY KEY (`id`, `guilds_id`),\n" +
+            "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,\n" +
+            "  INDEX `fk_guild_vaults_guilds1_idx` (`guilds_id` ASC) VISIBLE,\n" +
+            "  CONSTRAINT `fk_guild_vaults_guilds1`\n" +
+            "    FOREIGN KEY (`guilds_id`)\n" +
+            "    REFERENCES `guilds` (`id`)\n" +
+            "    ON DELETE NO ACTION\n" +
+            "    ON UPDATE NO ACTION)\n" +
+            "ENGINE = InnoDB;\n";
 
 /*    @Language("MySQL")
     public static final String ADD_NEW_GUILD = "INSERT IGNORE INTO guilds (id, name, prefix, " +
